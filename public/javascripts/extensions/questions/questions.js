cb61b41a78761117f96d188b87daf1abca2daa5b
cheef <cheef@cheef-laptop.(none)>
2010-09-28 23:59:07 +0400

    improved js

--- public/javascripts/extensions/questions/questions.js
+++ public/javascripts/extensions/questions/questions.js
@@ -1,27 +1,59 @@
 var QuestionForm = Class.create({
-  form: null, answers: [],
+  container: null, answers: [],
 
   initialize: function(element) {
-    this.form = $(element);
-    this.parseForm();
+    this.container = $(element);
+    this.parseContainer();
     this.attachEvents();
   },
 
   attachEvents: function() {
     Event.addBehavior({
-      '.b-add-answer-link': QuestionForm.Buttons.Behavior(this.form)
+      '.b-add-answer-link': QuestionForm.AddButtons.Behavior(this)
     });
   },
 
-  addAnswer: function(element) {
-    self.answers.push(new QuestionForm.Answer(element));
+  detectAnswer: function(container) {
+    var answer = new QuestionForm.Answer(container);
+
+    container.childElements().each(function(input) {
+      answer.detectPossibleAttributes(input);
+    });
+
+    return answer;
   },
 
-  parseForm: function() {
+  parseContainer: function() {
+    this.detectAnswers();
+  },
+
+  detectAnswers: function() {
     var self = this;
-    $$('.b-answers-column .b-answer').each(function(element) {
-      self.addAnswer(element);
+    $$('.b-answers-column .b-answer').each(function(answerContainer) {
+      self.addAnswer(self.detectAnswer(answerContainer));
     });
+  },
+
+  addAnswer: function(answer) {
+    return (answer ? this.answers.push(answer) : false);
+  },
+
+  removeAnswer: function(element) {
+    alert('test');
+  },
+
+  buildAnswer: function() {
+    var bodyLayout       = $div({'class': 'l-answer-body'});
+    var bodyInput        = $input({'class': 'b-answer-body', 'type': 'text', 'name': 'question[answer_attributes][][body]'});
+    var answerContainer  = $div({'class': 'b-answer'});
+    var answersContainer = $$('.b-answers-column').first();
+
+    bodyLayout.insert({bottom: bodyInput});
+    answerContainer.insert({bottom: $a({'class': 'b-remove-answer-link'})});
+    answerContainer.insert({bottom: bodyLayout});
+    answersContainer.insert({bottom: answerContainer});
+
+    this.addAnswer(new QuestionForm.Answer(answerContainer));
   }
 });
 
@@ -31,27 +63,77 @@ QuestionForm.Behavior = Behavior.create({
   }
 });
 
-QuestionForm.Buttons = {};
-QuestionForm.Buttons.Behavior = Behavior.create({
-  $form: null,
+QuestionForm.AddButtons = {};
+QuestionForm.AddButtons.Behavior = Behavior.create({
+  answer: null,
 
   initialize: function(form) {
     this.form = form;
   },
 
-  onclick: function(e) {
-    this.form.addAnswer();
+  onclick: function(event) {
+    this.form.buildAnswer();
+  }
+});
+
+QuestionForm.RemoveButton = {};
+QuestionForm.RemoveButton.Behavior = Behavior.create({
+  answer: null,
+
+  initialize: function(answer) {
+    this.answer = answer;
+  },
+
+  onclick: function(event) {
+    this.answer.remove();
   }
 });
 
 QuestionForm.Question = Class.create();
 
 QuestionForm.Answer = Class.create({
-  container: null, id: null, text: null,
+  container: null, id: null, body: null,
 
   initialize: function(element) {
     this.container = element;
-    console.log(element);
+    this.attachEvents();
+  },
+
+  isNewRecord: function() {
+    return this.id === null;
+  },
+
+  detectPossibleAttributes: function(input) {
+    return this.detectId(input) || this.detectBody(input);
+  },
+
+  detectId: function(input) {
+    this.detectAttribute(input, 'id');
+  },
+
+  detectBody: function(input) {
+    this.detectAttribute(input, 'body');
+  },
+
+  detectAttribute: function(input, attributeName) {
+    if (input.hasClassName('b-answer-' + attributeName)){
+      this[attributeName] = input.getValue();
+    }
+  },
+
+  remove: function() {
+    if (this.isNewRecord()) {
+      this.container.remove();
+    } else {
+      this.container.insert($input({'type': 'hidden','name': 'question[answer_attributes][' + this.id + '][should_remove]', 'value': 1}));
+      this.container.hide();
+    }
+  },
+
+  attachEvents: function() {
+    Event.addBehavior({
+      '.b-remove-answer-link': QuestionForm.RemoveButton.Behavior(this)
+    });
   }
 });
 
